51a2a4888d0e30b2d0d47afb7a72f5c7b26066ea
andherre <101904585+andherre@users.noreply.github.com>
2022-06-23 12:54:53 -0500

    Update reducer.py

--- pregunta_07/reducer.py
+++ pregunta_07/reducer.py
@@ -1,3 +1,21 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+import sys
+import operator
+
+if __name__ == '__main__':
+
+    tripletas = []
+
+    for line in sys.stdin:
+
+        key, val1, val2 = line.split(",")
+        val2 = int(val2)
+        tripletas_line = (key, val1, val2)
+        tripletas.append(tripletas_line)
+
+    tripletas_ord = sorted(tripletas, key = operator.itemgetter(0, 2))
+
+    for campo in tripletas_ord:
+        sys.stdout.write("{}   {}   {}\n".format(campo[0], campo[1], campo[2]))
