463a9d5f2370bddaffab2d33aa11b35c5c859f3b
andherre <101904585+andherre@users.noreply.github.com>
2022-06-23 12:55:04 -0500

    Update reducer.py

--- pregunta_03/reducer.py
+++ pregunta_03/reducer.py
@@ -1,3 +1,20 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+import sys
+import operator
+
+if __name__ == '__main__':
+
+    diccio = {}
+
+    for line in sys.stdin:
+
+        key, val = line.split("\t")
+        val = int(val)
+        diccio[key] = val
+
+    diccio_ord = sorted(diccio.items(), key = operator.itemgetter(1))
+
+    for campo in diccio_ord:
+        sys.stdout.write("{},{}\n".format(campo[0], campo[1]))
