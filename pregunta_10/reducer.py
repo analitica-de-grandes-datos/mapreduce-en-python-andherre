ed8b6926195a989535b6cdb05eb4d0eabd167f9a
andherre <101904585+andherre@users.noreply.github.com>
2022-06-23 12:54:41 -0500

    Update reducer.py

--- pregunta_10/reducer.py
+++ pregunta_10/reducer.py
@@ -1,3 +1,25 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+import sys
+from collections import defaultdict
+import operator
+
+if __name__ == '__main__':
+
+    diccio = defaultdict(list)
+
+    for line in sys.stdin:
+
+        numero, val = line.replace("\n","").strip().split("	")
+        val = val.split(",")
+        numero = int(numero)
+        for elemento in val:
+            diccio[elemento].append(numero)
+
+    diccio_ord = sorted(diccio.items(), key = operator.itemgetter(0))
+
+    for clave, valor in diccio_ord:
+        datos_num = sorted(valor, key = int)
+        valor = ",".join(map(str, datos_num))
+        sys.stdout.write("{}	{}\n".format(clave, valor))
